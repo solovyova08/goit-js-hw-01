1373e1c0ed7013eb49c47715f9bfad6fb97f5d5c
Inesa Soloviova <inesa200088@gmail.com>
2024-04-24 18:52:19 +0300

    js-homework

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function makeTransaction(quantity, pricePerDroid) {
+return message = `You ordered ${quantity} droids worth ${pricePerDroid}`;
+}
+
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
